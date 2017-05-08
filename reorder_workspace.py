921b9245e2d2079e472c3145c078421b1e37ef47
jli <jli@circularly.org>
2017-05-08 11:31:18 -0400

    reorder_workspace: crappy hack to avoid using existing names

--- reorder_workspace.py
+++ reorder_workspace.py
@@ -8,18 +8,27 @@ Only works for numbered workspaces currently.
 from __future__ import print_function, unicode_literals
 import argparse
 import itertools
+import random
 import re
 
 import i3ipc as i3
+import random_workspace
 
 
-def _swap_name_numbers(ws1, ws2):
+def _swap_numbers(ws1, ws2, all_workspaces):
   """Returns new workspace names with number prefix switched."""
   new_ws1_name = re.sub('^[0-9]+', str(ws2['num']), ws1['name'])
   new_ws2_name = re.sub('^[0-9]+', str(ws1['num']), ws2['name'])
-  return new_ws1_name, new_ws2_name
+  used_names = frozenset(ws['name'] for ws in all_workspaces)
+  def _avoid_used(new_name):
+    while new_name in used_names:
+      new_name = '{}__{}'.format(new_name,
+                                 random.choice(random_workspace.WORDS))
+    return new_name
+  return _avoid_used(new_ws1_name),  _avoid_used(new_ws2_name)
 
 
+# TODO: need to avoid trying to rename to an existing name.
 def _add_number_to_workspace(workspace, all_workspaces):
   """Adds a number prefix to the workspace name."""
   max_num = i3.max_workspace_number(all_workspaces)
@@ -31,14 +40,15 @@ def _add_number_to_workspace(workspace, all_workspaces):
 
 def _reorder_workspaces(prev, debug=False):
   """Reorders adjacent workspaces by renaming and swapping their numbers."""
-  all_ws = i3.focused_output_workspaces()
+  all_ws = i3.get_workspaces()
+  output_ws = i3.focused_output_workspaces(all_ws)
 
-  focused = i3.focused_workspace(all_ws)
+  focused = i3.focused_workspace(output_ws)
   if focused['num'] == -1:
-    _add_number_to_workspace(focused, all_ws)
+    _add_number_to_workspace(focused, output_ws)
     return
 
-  numbered_ws = [ws for ws in all_ws if ws['num'] != -1]
+  numbered_ws = [ws for ws in output_ws if ws['num'] != -1]
   if debug: print('numbered workspaces:', numbered_ws)
   # Add buffer for wrapping.
   ws = list(itertools.chain([numbered_ws[-1]], numbered_ws, [numbered_ws[0]]))
@@ -49,7 +59,7 @@ def _reorder_workspaces(prev, debug=False):
   for (ws1, ws2) in workspace_pairs:
     if debug: print('checking <{}>  vs  <{}>'.format(ws1['name'], ws2['name']))
     if ws1['focused']:
-      new_ws1_name, new_ws2_name = _swap_name_numbers(ws1, ws2)
+      new_ws1_name, new_ws2_name = _swap_numbers(ws1, ws2, all_ws)
       # TODO: sending 2 renames in 1 command causes weird inconsistencies. is
       # that expected?
       i3.command('rename', 'workspace', ws1['name'], 'to', new_ws1_name)
